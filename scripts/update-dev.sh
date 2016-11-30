95052c4bc54ccb15a9d4727cb143d936db20b342
john arroyo <john.arroyo@gmail.com>
2016-11-29 16:22:15 -0800

    add colored status messages after each pull

--- scripts/update-dev.sh
+++ scripts/update-dev.sh
@@ -7,21 +7,29 @@ BRANCH="develop"
 DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 cd $DIR
 
+# Add some color bling
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
 # Pull user-admin
 git checkout $BRANCH
 git pull origin $BRANCH
+echo "${GREEN}user-admin updated\n ${RESET}"
 
-# Pull authenticate
-cd ../../authenticate
+# Pull users
+cd ../../users
 git checkout $BRANCH
 git pull origin $BRANCH
+echo "${GREEN}users updated\n ${RESET}"
+
+# Pull authenticate
+cd ../authenticate
+git checkout $BRANCH
+git pull origin $BRANCH
+echo "${GREEN}authenticate updated\n ${RESET}"
 
 # Pull authorize
 cd ../authorize
 git checkout $BRANCH
 git pull origin $BRANCH
-
-# Pull users
-cd ../users
-git checkout $BRANCH
-git pull origin $BRANCH
+echo "${GREEN}authorize updated\n ${RESET}"

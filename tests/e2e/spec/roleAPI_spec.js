9960d4ca543c0d2349ce0e4cc862f7c8e5e3742b
Julian Diaz <julian@arroyolabs.com>
2016-12-15 16:59:05 -0300

    added some identation

--- tests/e2e/spec/roleAPI_spec.js
+++ tests/e2e/spec/roleAPI_spec.js
@@ -1,32 +1,36 @@
 var frisby = require('../node_modules/frisby/lib/frisby');
 
-var baseURL = 'http://docker.local:8088/ajax/users/';
+var baseURL = process.env.baseURL;
 
 describe('Role api test suite', function() {
-/**----------------------------------------------------------------*/
-/**-----------------Role not found---------------------------------*/
-frisby.create('Role with ID 999999999 not found')
-      .get(baseURL + 'role?id=999999999')
-      .expectStatus(200)
-      .afterJSON(function (response) {
-       		expect(response.body.method).toMatch('role');
-       		expect(response.body.success).toMatch('false');
-       		expect(response.body.error_message).toMatch('Role not found.');       		
-  	   })
-  	       
-      .toss()
 
-/**----------------------------------------------------------------*/
-/**-----------------Get all roles----------------------------------*/
-frisby.create('Get all roles')
-      .get(baseURL + 'roles')
-      .expectStatus(200)
-      .expectHeader('Content-Type', 'text/html; charset=utf-8')      
-      .afterJSON(function (response) {          
-          expect(response.body.method).toBe('roles');
-          expect(response.body.success).toBe(true);          
-          expect(response.errors).toBe(false);
-          expect(response.body.roles).toBeDefined();
-       })     
-      .toss()
-});
+  beforeEach(function() {                
+  });
+
+  /**----------------------------------------------------------------*/
+  /**-----------------Role not found---------------------------------*/
+  frisby.create('Role with ID 999999999 not found')
+        .get(baseURL + 'role?id=999999999')
+        .expectStatus(200)
+        .afterJSON(function (response) {
+       	  	expect(response.body.method).toMatch('role');
+       		  expect(response.body.success).toMatch('false');
+       		  expect(response.body.error_message).toMatch('Role not found.');       		
+  	    })
+  	       
+        .toss()
+
+  /**----------------------------------------------------------------*/
+  /**-----------------Get all roles----------------------------------*/
+  frisby.create('Get all roles')
+        .get(baseURL + 'roles')
+        .expectStatus(200)
+        .expectHeader('Content-Type', 'text/html; charset=utf-8')      
+        .afterJSON(function (response) {          
+            expect(response.body.method).toBe('roles');
+            expect(response.body.success).toBe(true);          
+            expect(response.errors).toBe(false);
+            expect(response.body.roles).toBeDefined();
+        })     
+        .toss()
+  });

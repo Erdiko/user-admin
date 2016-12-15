e6c23836c4ac80c3d9b34d3d77705330fc4d7d8c
Julian Diaz <julian@arroyolabs.com>
2016-12-15 18:40:18 -0300

    fixe route.

--- tests/e2e/spec/roleAPI_spec.js
+++ tests/e2e/spec/roleAPI_spec.js
@@ -1,6 +1,6 @@
 var frisby = require('../node_modules/frisby/lib/frisby');
 
-var baseURL = process.env.baseURL + '/_users/roles/';
+var baseURL = process.env.baseURL + '/users/roles/';
 
 describe('Role api test suite', function() {
 

6a99250ca45ddc492ff7cf3eab948cc89a1108ab
Andy Armstrong <andyarmstro@gmail.com>
2016-12-20 23:33:32 -0800

    ER-95: fixed issue where dist wasnt going to cut it for generated files ; updated theme files to use moved / safe assets

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -1,20 +1,3 @@
 <div class="container">
-  <div class="page-header" id="banner">
-    <div class="row">
-      <div class="col-lg-8 col-md-7 col-sm-6">
-        <h1>Welcome to Erdiko Auth Module</h1>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-4 col-md-5 col-sm-6 col-xs-10">
-        <h1>Welcome <?php echo $data["user"]->getDisplayName(); ?></h1>
-        <h3>your Roles:</h3>
-        <ul>
-        <?php foreach ($data["user"]->getRoles() as $role): ?>
-          <li><?php echo $role; ?></li>
-        <?php endforeach; ?>
-        </ul>
-      </div>
-    </div>
-  </div>
+    <app-root>Loading...</app-root>
 </div>

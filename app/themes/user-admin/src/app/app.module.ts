bebc4626962b074829c45fb9bf01e2686dcfdd4b
Andy Armstrong <andyarmstro@gmail.com>
2016-12-20 23:37:16 -0800

    ER-95: added new component as a test

--- app/themes/user-admin/src/app/app.module.ts
+++ app/themes/user-admin/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,

56f88f855afcb7bff93c6106a3bc3ae704714944
Andy Armstrong <andyarmstro@gmail.com>
2016-12-21 10:07:15 -0800

    added sass to project

--- app/themes/user-admin/src/app/user-list/user-list.component.ts
+++ app/themes/user-admin/src/app/user-list/user-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
 

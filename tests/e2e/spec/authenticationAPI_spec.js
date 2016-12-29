988c13dfa3fa24cfb81c97a5f927baaf9cc7d3c6
Julian Diaz <julian@arroyolabs.com>
2016-12-29 14:45:06 -0300

    changes to support Mailgun.

--- tests/e2e/spec/authenticationAPI_spec.js
+++ tests/e2e/spec/authenticationAPI_spec.js
@@ -147,4 +147,63 @@ describe('Authentication api test suite', function() {
                 .toss()
         })
         .toss()
+
+
+    /**----------------------------------------------------------------*/
+    /**-----------------Forgot password test -------------------------------*/
+    frisby.create('Login.')
+        .post(userURL + 'register',
+            newUser,
+            { json: true },
+            post_header)
+        .expectStatus(200)
+        .expectJSONTypes(
+            {
+                "status": Number,
+                "body":{
+                    "method": String,
+                    "success": Boolean,
+                    "user":{
+                        "id":Number,
+                        "email":String,
+                        "name":String,
+                        "last_login":null,
+                        "gateway_customer_id":null
+                    },
+                    "error_message": String,
+                    "error_code": Number
+                },
+                "errors": Boolean
+            })
+        .afterJSON(function (response) {
+            expect(response.body.method).toBe('register');
+            expect(response.body.success).toBe(true);
+            var USER_ID = response.body.user.id.toString();
+            var USER_EMAIL = newUser.email;
+            var USER_PASS = newUser.password;
+            /**--checking forgot --*/
+            frisby.create('forgot pass')
+                .post(baseURL + 'forgotpass',
+                    {"email":USER_EMAIL},
+                    { json: true },
+                    post_header)
+                .expectStatus(200)
+                .afterJSON(function (response) {
+                    expect(response.body.method).toBe('forgotpass');
+                    expect(response.body.success).toBe(true);
+                    expect(response.body.error_message).toBe("");
+                })
+                .toss()
+            /**--delete user created --*/
+            frisby.create('removing user created.')
+                .get(userURL + 'cancel?id=' + USER_ID)
+                .expectStatus(200)
+                .afterJSON(function (response) {
+                    expect(response.body.method).toBe('cancel');
+                    expect(response.body.success).toBe(true);
+                    expect(response.body.user.id).toBe(USER_ID);
+                })
+                .toss()
+        })
+        .toss()
 });

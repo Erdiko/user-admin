dc707b38448c5802a8262092d778354eb758b902
john arroyo <john.arroyo@gmail.com>
2017-02-19 00:28:01 -0800

    bump up php version to 5.5; tweak the ci-test scripts

--- scripts/ci-tests.sh
+++ scripts/ci-tests.sh
@@ -4,6 +4,10 @@
 GREEN='\033[0;32m'
 RESET='\033[0m'
 
+# cd to the script folder (so that the script can be ran anywhere)
+DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+cd $DIR
+
 # Run unit tests inside of docker (3 test suites)
 ../vendor/erdiko/users/scripts/ci-tests.sh
 echo "${GREEN}ran unit test suite for users package\n ${RESET}"

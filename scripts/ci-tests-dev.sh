d776316f488517ce243f616269c7dabf326f29a9
john arroyo <john.arroyo@gmail.com>
2017-02-19 00:45:42 -0800

    update ci-test to run from within container (absolute path)

--- scripts/ci-tests-dev.sh
+++ scripts/ci-tests-dev.sh
@@ -5,11 +5,11 @@ GREEN='\033[0;32m'
 RESET='\033[0m'
 
 # Run unit tests inside of docker (3 test suites)
-/code/vendor/erdiko/users/scripts/ci-tests.sh
+../vendor/erdiko/users/scripts/ci-tests.sh
 echo "${GREEN}ran unit test suite for users package\n ${RESET}"
 
-/code/vendor/erdiko/authorize/scripts/ci-tests.sh
+../vendor/erdiko/authorize/scripts/ci-tests.sh
 echo "${GREEN}ran unit test suite for authorize package\n ${RESET}"
 
-/code/vendor/erdiko/authenticate/scripts/ci-tests.sh
+../vendor/erdiko/authenticate/scripts/ci-tests.sh
 echo "${GREEN}ran unit test suite for authenticate package\n ${RESET}"

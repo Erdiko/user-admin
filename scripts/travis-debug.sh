8a89c327260eaa4d6f27532b6a2a4e7a27399b70
john arroyo <john.arroyo@gmail.com>
2017-02-21 13:48:29 -0800

    try a couple of new IPs for travis

--- scripts/travis-debug.sh
+++ scripts/travis-debug.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-mysql -u root -ppsItPs16 -h 172.17.0.1 -P 3301 -e 'SET GLOBAL wait_timeout = 36000;' user-admin
-mysql -u root -ppsItPs16 -h 172.17.0.1 -P 3301 -e 'SET GLOBAL max_allowed_packet = 33554432;' user-admin 
-
 echo "Try to query @ 172.17.0.1"
 mysql -u root -ppsItPs16 -h 172.17.0.1 -P 3301 -e 'show tables;' user-admin 
+
+echo "Try to query @ 172.18.0.2"
+mysql -u root -ppsItPs16 -h 172.18.0.2 -P 3301 -e 'show tables;' user-admin 
+
+echo "Try to query @ 172.18.0.3"
+mysql -u root -ppsItPs16 -h 172.18.0.3 -P 3301 -e 'show tables;' user-admin 
+
+
+
+#mysql -u root -ppsItPs16 -h erdiko_database -P 3301 -e 'show tables;' user-admin 
+#mysql -u root -ppsItPs16 -h database -P 3301 -e 'show tables;' user-admin 
+#mysql -u root -ppsItPs16 -h db -P 3301 -e 'show tables;' user-admin 

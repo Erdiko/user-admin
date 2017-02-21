f6aac309097e98973e9f87afc61d86d0b3f30388
john arroyo <john.arroyo@gmail.com>
2017-02-20 16:59:19 -0800

    try adjusting the mysql settings

--- scripts/travis-debug.sh
+++ scripts/travis-debug.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+mysql -u root -ppsItPs16 -h 172.17.0.1 -P 3301 -e 'SET GLOBAL wait_timeout = 36000;' user-admin
+mysql -u root -ppsItPs16 -h 172.17.0.1 -P 3301 -e 'SET GLOBAL max_allowed_packet = 33554432;' user-admin 
+
 echo "Try to query @ 172.17.0.1"
-mysql -u root -ppsItPs16 -h 172.17.0.1 -P 3301 -e 'show tables' user-admin 
+mysql -u root -ppsItPs16 -h 172.17.0.1 -P 3301 -e 'show tables;' user-admin 

026da0cd23bbad894b696701768f557355a7074c
john arroyo <john.arroyo@gmail.com>
2017-02-23 01:34:53 -0800

    try to refer to container by name

--- scripts/install-db.sh
+++ scripts/install-db.sh
@@ -4,8 +4,8 @@
 GREEN='\033[0;32m'
 RESET='\033[0m'
 
-mysql -u root -ppsItPs16 -h 172.17.0.1 -P 3301 < /code/scripts/sql/create-db.sql
+mysql -u root -ppsItPs16 -h erdiko_users_db < /code/scripts/sql/create-db.sql
 echo "${GREEN}Create database\n ${RESET}"
 
-mysql -u root -ppsItPs16 -h 172.17.0.1 -P 3301 user-admin < /code/vendor/erdiko/users/sql/dumps/user-admin.sql
+mysql -u root -ppsItPs16 -h erdiko_users_db user-admin < /code/vendor/erdiko/users/sql/dumps/user-admin.sql
 echo "${GREEN}update database schema\n ${RESET}"

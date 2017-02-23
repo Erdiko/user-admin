3cc03bfcc4d223d3266ac9bf5a93de151a7eaa71
john arroyo <john.arroyo@gmail.com>
2017-02-23 01:46:05 -0800

    refer to mysql by service name (db)

--- scripts/travis-debug.sh
+++ scripts/travis-debug.sh
@@ -4,10 +4,13 @@ echo "Try to query @ erdiko_users_db"
 mysql -u root -ppsItPs16 -h erdiko_users_db -e 'show tables;' user-admin
 
 echo "Try to query @ erdiko_users_db:3301"
-mysql -u root -ppsItPs16 -h erdiko_users_db 3301 -e 'show tables;' user-admin
+mysql -u root -ppsItPs16 -h erdiko_users_db -P 3301 -e 'show tables;' user-admin
 
 echo "Try to query @ db"
-mysql -u root -ppsItPs16 -h db -P -e 'show tables;' user-admin
+mysql -u root -ppsItPs16 -h db -e 'show tables;' user-admin
+
+echo "Try to query @ db:3301"
+mysql -u root -ppsItPs16 -h db -P 3301 -e 'show tables;' user-admin
 
 
 #mysql -u root -ppsItPs16 -h erdiko_database -P 3301 -e 'show tables;' user-admin 

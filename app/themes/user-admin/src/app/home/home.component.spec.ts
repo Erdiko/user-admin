5f48a1ce75122b4555aa92b0c30a0b8e79393082
Andy Armstrong <andyarmstro@gmail.com>
2017-03-01 12:41:38 -0800

    ER-112: updated home component test

--- app/themes/user-admin/src/app/home/home.component.spec.ts
+++ app/themes/user-admin/src/app/home/home.component.spec.ts
@@ -6,24 +6,38 @@ import { DebugElement } from '@angular/core';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ HomeComponent ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should display an h1', () => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('Welcome to the Erdiko User Admin');
+    });
+
+    it('should display an ul element with 2 links', () => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+
+        expect(compiled.querySelectorAll('ul')).toBeTruthy();
+        expect(compiled.querySelectorAll('ul li').length).toBe(2);
+    });
 
 });

ff348c0416cb600751b27e961ca503417f36068a
Andy Armstrong <andyarmstro@gmail.com>
2017-03-01 21:43:34 -0800

    ER-145: more clean up

--- app/themes/user-admin/src/app/login/login.component.ts
+++ app/themes/user-admin/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit }                    from '@angular/core';
-import { Router, ActivatedRoute }               from '@angular/router';
+import { Router }                               from '@angular/router';
 import { FormBuilder, FormGroup, Validators }   from '@angular/forms';
 
 import { AuthService }   from '../shared/auth.service';
@@ -21,7 +21,6 @@ export class LoginComponent implements OnInit {
 
     constructor(
            private authService: AuthService,
-           private route: ActivatedRoute,
            private router: Router,
            private fb: FormBuilder) { 
 

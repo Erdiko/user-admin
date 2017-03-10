b2b182176ea1b4729e092dfcbe7dbc354f2212ef
kkennethlee <kyung.kenneth.lee@gmail.com>
2017-03-10 00:34:36 -0800

    commiting before fetch/merge

--- app/themes/user-admin/src/app/shared/users.service.ts
+++ app/themes/user-admin/src/app/shared/users.service.ts
@@ -200,7 +200,7 @@ export class UsersService {
      * get event logs for a user
      *
      */
-    getUserEvents(id: string, pagesize?: number, page?: number, sortCol?: string, sortDir?: string) {
+    getUsersEvents(id: string, pagesize?: number, page?: number, sortCol?: string, sortDir?: string) {
 
         let url = this._baseUrl + this.userEventUrl;
 

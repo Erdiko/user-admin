f4628909946e2894505bbb39bc40bf02b66677d5
Andy Armstrong <andyarmstro@gmail.com>
2017-03-15 21:06:56 -0700

    finally can test logged out & logged in
    Finally got the mocked service to cooperate with spyOn

--- app/themes/user-admin/src/app/header/header.component.spec.ts
+++ app/themes/user-admin/src/app/header/header.component.spec.ts
@@ -14,11 +14,18 @@ import { UserResolve }     from '../shared/user-resolve.service';
 
 import { HeaderComponent } from './header.component';
 
+class MockAuthService extends AuthService {
+    isLoggedIn() {
+        return false;
+    }
+}
+
 describe('HeaderComponent', () => {
     let component: HeaderComponent;
     let fixture: ComponentFixture<HeaderComponent>;
 
     beforeEach(async(() => {
+
         TestBed.configureTestingModule({
             declarations: [ 
                 HeaderComponent 
@@ -28,7 +35,10 @@ describe('HeaderComponent', () => {
                 RouterTestingModule
             ],
             providers: [
-                AuthService,
+                {
+                    provide: AuthService, 
+                    useClass: MockAuthService
+                },
                 UsersService,
                 UserResolve
             ]
@@ -53,11 +63,26 @@ describe('HeaderComponent', () => {
         expect(compiled.querySelectorAll('.navbar.navbar-inverse')).toBeTruthy();
     });
 
-    it('should display an ul element with 3 links', () => {
+    it('should display an ul element with 0 links when notlogged in', () => {
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
 
-        console.log(compiled);
+        expect(compiled.querySelectorAll('ul')).toBeTruthy();
+        expect(compiled.querySelectorAll('ul li').length).toBe(0);
+    });
+
+    it('should display an ul element with 3 links when logged in', () => {
+        
+        /*
+         * Get the mocked service here from our fixture
+         * and add a spyOn over-ride to pretend we have
+         * a logged in user.
+         */
+        let service = fixture.debugElement.injector.get(AuthService);
+        spyOn(service, 'isLoggedIn').and.returnValue(true);
+
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
 
         expect(compiled.querySelectorAll('ul')).toBeTruthy();
         expect(compiled.querySelectorAll('ul li').length).toBe(3);

781e52e2c229f0bd43fa43c3bb4f366d695ca28a
Andy Armstrong <andyarmstro@gmail.com>
2017-06-02 23:20:52 -0700

    moved more stuff to the pkg

--- app/themes/user-admin/src/app/app.module.ts
+++ app/themes/user-admin/src/app/app.module.ts
@@ -1,51 +1,44 @@
 import { BrowserModule }        from '@angular/platform-browser';
 import { NgModule }             from '@angular/core';
+
 import { FormsModule, 
          ReactiveFormsModule }  from '@angular/forms';
+
+import { RouterModule, Routes }     from '@angular/router';
+
 import { HttpModule }           from '@angular/http';
-
-import { AppComponent }         from './app.component';
-import { AppRoutingModule }     from './app.routing';
-
-import { AuthService }          from './shared/auth.service';
-import { UserResolve }          from './shared/user-resolve.service';
-
-import { LoginComponent }       from './login/login.component';
-import { HomeComponent }        from './home/home.component';
-
-import { HeaderComponent }      from './header/header.component';
 
 import { AlertModule, 
          ModalModule,
          TabsModule }          from 'ngx-bootstrap';
 
 import { UserAdminModule }      from '@erdiko/ng-user-admin';
-import { UsersService }         from './shared/users.service';
+
+import { AppComponent }         from './app.component';
+
+// clang-format off
+const routes: Routes = [];
+// clang-format on
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    AppRoutingModule,
 
     AlertModule.forRoot(),
     ModalModule.forRoot(),
     TabsModule.forRoot(),
 
+    RouterModule.forRoot(routes),
+
     UserAdminModule.forRoot()
   ],
   providers: [
-    AuthService,
-    UserResolve,
-    UsersService
   ],
   bootstrap: [AppComponent]
 })

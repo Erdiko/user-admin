5defff40e9a94002a71cca4160b20e602da44ef5
john arroyo <john.arroyo@gmail.com>
2017-06-22 13:57:24 -0700

    update msg

--- scripts/install-db.sh
+++ scripts/install-db.sh
@@ -8,7 +8,7 @@ mysql -u root -ppsItPs16 -h db < /code/scripts/sql/create-db.sql
 echo "${GREEN}Create database\n ${RESET}"
 
 mysql -u root -ppsItPs16 -h db user-admin < /code/vendor/erdiko/users/sql/dumps/user-admin.sql
-echo "${GREEN}update database schema\n ${RESET}"
+echo "${GREEN}Update database schema\n ${RESET}"
 
 # execute the create-user script
 php /code/vendor/erdiko/users/scripts/create-users.php -b=/code/app/bootstrap.php

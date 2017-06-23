f9b33356aba89d75aaa7bffc5226aee06a0c2698
john arroyo <john.arroyo@gmail.com>
2017-06-23 01:24:51 -0700

    don’t run the install-web script

--- scripts/ci/install-web.sh
+++ scripts/ci/install-web.sh
@@ -6,4 +6,4 @@ rm /etc/nginx/fastcgi.conf
 cp /code/docker/nginx/site.conf /etc/nginx/conf.d/site.conf
 cp /code/docker/nginx/fastcgi.conf /etc/nginx/fastcgi.conf
 
-echo "Done update nginx config"
+echo "Done updating nginx config"

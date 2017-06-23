76cda64871d57a76826274c22263eb6e2797cd57
john arroyo <john.arroyo@gmail.com>
2017-06-23 01:10:12 -0700

    fix nginx path

--- scripts/ci/install-web.sh
+++ scripts/ci/install-web.sh
@@ -3,7 +3,7 @@
 rm /etc/nginx/conf.d/site.conf
 rm /etc/nginx/fastcgi.conf
 
-cp site.conf /etc/nginx/conf.d/site.conf
-cp fastcgi.conf /etc/nginx/fastcgi.conf
+cp /code/docker/nginx/site.conf /etc/nginx/conf.d/site.conf
+cp /code/docker/nginx/fastcgi.conf /etc/nginx/fastcgi.conf
 
 echo "Done update nginx config"

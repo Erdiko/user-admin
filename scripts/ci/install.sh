a52869b04cf901b4e7b94e4ce84a6df07b9f3320
john arroyo <john.arroyo@gmail.com>
2017-06-22 17:37:06 -0700

    try fix session strorage test
    error:
    
    4) tests\phpunit\services\SessionStorageTest::testSessionPersist
    mkdir(): No such file or directory
    
    /code/vendor/erdiko/authenticate/src/services/SessionStorage.php:59
    /code/vendor/erdiko/authenticate/src/services/SessionStorage.php:20
    /code/vendor/erdiko/authenticate/tests/phpunit/services/SessionStorageTest.php:55

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -1,38 +1,11 @@
 #!/bin/sh
 
-# set branch and use everywhere
-BRANCH="develop"
+/code/scripts/ci/update-composer.sh
 
-# Add some color bling
-GREEN='\033[0;32m'
-RESET='\033[0m'
+/code/scripts/ci/update-vendor.sh
 
-pwd
-ls -la
+/code/scripts/install-db.sh
 
-mkdir repos
-cd repos
-
-# Clone users
-git clone git@github.com:Erdiko/users.git
-cd ./users
-git checkout $BRANCH
-echo "${GREEN}users updated\n ${RESET}"
-
-# Clone authenticate
-cd ../
-git clone git@github.com:Erdiko/authenticate.git
-cd ./authenticate
-git checkout $BRANCH
-echo "${GREEN}authenticate updated\n ${RESET}"
-
-# Clone authorize
-cd ../
-git clone git@github.com:Erdiko/authorize.git
-cd ./authorize
-git checkout $BRANCH
-echo "${GREEN}authorize updated\n ${RESET}"
-
-cd ../
-pwd
-ls -la
+# Hack for CI
+mkdir /code/var
+chmod -R 777 /code/var

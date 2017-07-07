8eb056dcdcd6f531c7077194fa9d670560f2d9a9
john arroyo <john.arroyo@gmail.com>
2017-07-07 15:29:50 -0700

    hardcode the directory path

--- scripts/dev/update-composer.sh
+++ scripts/dev/update-composer.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # cd to the script folder (so that the script can be ran anywhere)
-DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
-cd $DIR
+# DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+# cd $DIR
 
 # Update composer packages
-cd ../../
-composer clearcache
+cd /code
 composer update

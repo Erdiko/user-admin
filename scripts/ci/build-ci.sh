310c6ca219fa71323a2649f1f566e77c30b39da9
john arroyo <john.arroyo@gmail.com>
2017-07-11 12:07:13 -0700

    add debug step

--- scripts/ci/build-ci.sh
+++ scripts/ci/build-ci.sh
@@ -2,5 +2,7 @@
 
 docker-compose -f docker-compose-ci.yml up -d
 
+docker ps -a
+
 docker exec -it erdiko_users_php /code/scripts/dev/update-composer.sh
 docker exec -it erdiko_users_php /code/scripts/install-db.sh

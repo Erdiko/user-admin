27c973ee9205acfd9141b16addabb2d0444c8175
john arroyo <john.arroyo@gmail.com>
2017-07-12 18:03:49 -0700

    fix cut and paste issues

--- scripts/ci/build-ci.sh
+++ scripts/ci/build-ci.sh
@@ -10,13 +10,13 @@ docker ps -a
 docker cp ./ erdiko_users_php:/code
 docker cp ./composer-dev.json erdiko_users_php:/code/composer.json
 docker cp ./repos/authenticate erdiko_users_php:/authenticate
-docker cp ./repos/authenticate erdiko_users_php:/authorize
-docker cp ./repos/authenticate/users erdiko_users_php:/users
+docker cp ./repos/authorize erdiko_users_php:/authorize
+docker cp ./repos/users erdiko_users_php:/users
 
 docker cp ./ erdiko_users_web:/code
 docker cp ./repos/authenticate erdiko_users_web:/authenticate
-docker cp ./repos/authenticate erdiko_users_web:/authorize
-docker cp ./repos/authenticate/users erdiko_users_web:/users
+docker cp ./repos/authorize erdiko_users_web:/authorize
+docker cp ./repos/users erdiko_users_web:/users
 
 docker exec -it erdiko_users_php /code/scripts/dev/update-composer.sh
 docker exec -it erdiko_users_php /code/scripts/install-db.sh

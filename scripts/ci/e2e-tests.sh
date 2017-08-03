a8dd8d616d2eb844a4e8a40b75f0aecf6a4124f4
Andy Armstrong <andyarmstro@gmail.com>
2017-08-02 18:13:22 -0700

    #83 try to run e2e against another docker instance

--- scripts/ci/e2e-tests.sh
+++ scripts/ci/e2e-tests.sh
@@ -2,4 +2,4 @@
 
 # Run e2e tests for user-admin inside of docker
 cd /code/app/themes/user-admin
-npm run e2e
+docker run -it --privileged --rm --net=host -v /dev/shm:/dev/shm -v $(pwd):/protractor webnicer/protractor-headless 
